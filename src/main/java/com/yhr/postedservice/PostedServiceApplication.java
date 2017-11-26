36bacf6d8fff2826e5f81ce62f5229d4c674fc79
yhr <1361579931@qq.com>
2017-11-26 22:34:40 +0800

    初始化代码

---
+++ src/main/java/com/yhr/postedservice/PostedServiceApplication.java
@@ -0,0 +1,12 @@
+package com.yhr.postedservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostedServiceApplication.class, args);
+	}
+}
